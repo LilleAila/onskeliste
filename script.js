9125f6e29cc0becac0d74f024dc870d67c1c9331
Lille Aila <67327023+LilleAila@users.noreply.github.com>
2020-08-28 09:43:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 $('document').ready(function() {
   $('ting').each(function(index) {
-    /*console.log(`
-      url: ${$(this).attr('url')} \n
-      img: ${$(this).attr('img')} \n \n      ————————————
-    `)*/
-    //console.log(index)
     $('ting tang').eq(index).html(`<txt>${$(this).attr('tekst')}</txt>`);
     $('ting tang').eq(index).append(`
       <img src="` + $(this).attr('img') + `"></img>
     `);
-    /*$('ting tang').eq(index).click(function() {
-      window.open($(this).attr('url'));
-    });*/
   });
 });
